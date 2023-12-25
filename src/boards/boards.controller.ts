1507de0fe3929a6f6e57dd2e6f1b78a4acbc301d
Bomin Kim <iambxmk@gmail.com>
2023-12-25 15:36:49 +0900

    모든 게시물 가져오는 service 만들기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { BoardsService } from './boards.service';
 
+/** /boards 경로로 들어옴 */
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+  constructor(private boardsService: BoardsService) {}
+
+  /** /boards/ 경로 요청에 대한 호출. @Get('/')과 동일 */
+  @Get()
+  getAllBoards() {
+    return this.boardsService.getAllBoards();
+  }
+}
