20c7f08fd1f4385e1c45a96f56b90f6b8975b891
Bomin Kim <iambxmk@gmail.com>
2023-12-28 22:44:09 +0900

    게시물을 위한 entity 생성하기

---
+++ src/boards/board.entity.ts
@@ -0,0 +1,28 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BoardStatus } from './board.model';
+
+@Entity() // board class가 entity임을 나타냄 (CREATE TABLE board 부분)
+export class Board extends BaseEntity {
+  @PrimaryGeneratedColumn() // board entity의 기본키임을 의미함
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: BoardStatus;
+}
+
+/**
+ * ORM 없이 기존 데이터베이스 테이블 생성을 아래처럼 해줘야 하는데
+ * TypeORM 사용하면 데이터베이스 테이블로 변환되는 class이기 때문에 위처럼 클래스 생성 후 컬럼 정의해주면 됨
+ *
+ * CREATE TABLE board {
+ *  id INTEGER AUTO_INCREMENT PRIMARY KEY
+ *  title VARCHAR(255) NOT NULL
+ *  description VARCHAR(255) NOT NULL
+ * }
+ */
