e9c23e45a5eb4ac1ed139dafb7527f163a130def
Bomin Kim <iambxmk@gmail.com>
2023-12-31 14:32:48 +0900

    회원가입 기능 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,34 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 
 @Controller('auth')
 export class AuthController {
+  /** AuthService 를 사용하기 위한 종속성 주입 */
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  @Post('/signup')
+  signUp(@Body() authCredentialDto: AuthCredentialDto): Promise<void> {
+    return this.authService.signUp(authCredentialDto);
   }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,10 +1,17 @@
 import { DataSource, Repository } from 'typeorm';
 import { User } from './entities/auth.entity';
 import { Injectable } from '@nestjs/common';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
   constructor(dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+    const user = this.create({ username, password });
+    await this.save(user);
   }
 }
