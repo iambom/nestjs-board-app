391620e2d7643a2c8adbbb17b17a7efc87367bae
Bomin Kim <iambxmk@gmail.com>
2023-12-31 14:42:00 +0900

    유저 데이터 유효성 체크

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,15 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  /** 영어랑 숫자만 입력 가능하게 하는 정규식 */
+  @Matches(/^[a-zA-z0-9]*$/, { message: '영어랑 숫자만 입력하시오' })
   password: string;
 }
