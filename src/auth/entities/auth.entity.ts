a9fba35a42fbfc868012ddd5a551fc0bf533965d
Bomin Kim <iambxmk@gmail.com>
2023-12-31 13:59:39 +0900

    Auth Entity 생성

--- src/auth/entities/auth.entity.ts
+++ src/auth/entities/auth.entity.ts
@@ -1,1 +1,13 @@
-export class Auth {}
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity() // 이 클래스가 entity 클래스인 걸 지정해주는 데코레이터
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+}
