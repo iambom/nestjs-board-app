203908d851e2976d79814fee2cac2707d772628f
Bomin Kim <iambxmk@gmail.com>
2023-12-31 16:55:11 +0900

    JWT를 이용해서 토큰 생성하기

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserRepository } from './user.repository';
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'secret1234',
+      signOptions: {
+        expiresIn: 60 * 60,
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UserRepository],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
 import { UserRepository } from './user.repository';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import * as bcrypt from 'bcryptjs';
@@ -8,19 +9,27 @@ import * as bcrypt from 'bcryptjs';
 export class AuthService {
   /** AuthService에서 UserRepository 사용할 수 있도록 종속성 주입 */
   constructor(
-    @InjectRepository(UserRepository) private userRepository: UserRepository,
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
     return this.userRepository.createUser(authCredentialDto);
   }
 
-  async signIn(authCredentialDto: AuthCredentialDto): Promise<string> {
+  async signIn(
+    authCredentialDto: AuthCredentialDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialDto;
     const user = await this.userRepository.findOneBy({ username });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      return '로그인 성공';
+      // 유저 토큰 생성 (Secret text + Payload 필요)
+      const payload = { username };
+      const accessToken = this.jwtService.sign(payload); // sign 메소드에서 알아서 secret 과 payload를 합쳐서 accessToken 만들어줌
+
+      return { accessToken };
     } else {
       throw new UnauthorizedException('로그인 실패');
     }
